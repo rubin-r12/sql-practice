cf41513aeca795f60c3e9d553c4bba5670b1781c
Rubin <rubinradhakrishnan@gmail.com>
2024-08-13 22:12:18 +0530

    DML commands

--- queries.sql
+++ queries.sql
@@ -105,6 +105,227 @@ insert into employee_with_constraints_tmp values
 (1, 'Arjun', 500,'2024-02-02');
 --- Error Code: 3819. Check constraint 'salary_check' is violated.
 
+--- class2
+CREATE database class2_db;
+use class2_db;
+
+CREATE table if NOT EXISTS employee(
+id int,
+name VARCHAR(50),
+address VARCHAR(50),
+city VARCHAR(50)
+);
+
+INSERT into employee values(1,'Rubin','TNABR','Dewas');
+select * FROM employee;
+
+--- add new column to the table
+ALTER table employee add DOB DATE;
+
+--- modify column
+ALTER table employee modify address VARCHAR(100);
+
+show CREATE table employee;
+
+CREATE TABLE `employee` (
+  `id` int DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `DOB` date DEFAULT NULL
+);
+
+--- drop column
+ALTER table employee DROP column city;
+
+--- rename column
+alter table employee rename column name to full_name;
+
+--- drop employee table
+drop table employee;
+
+create table if not exists employee(
+	id int,
+    name varchar(50),
+    age int,
+    hiring_date date,
+    salary int,
+    city varchar(50)
+);
+
+insert into employee values(1,'A',28,'2018-06-18',83000,'Dewas');
+insert into employee values(2,'B',27,'2019-05-19',73000,'Indore');
+insert into employee values(3,'C',26,'2020-04-20',97000,'Ujjain');
+insert into employee values(4,'D',25,'2021-07-21',65000,'Dhar');
+insert into employee values(5,'E',24,'2022-02-23',53000,'Mhow');
+
+select * from employee;
+
+--- make id column unique
+alter table employee add constraint id_unique unique(id);
+
+show create table employee;
+
+--- delete constraint
+alter table employee drop constraint id_unique;
+
+--- primary key
+create table persons(
+	id int,
+    name varchar(50),
+    age int,
+    Primary Key(id)
+);
+
+insert into persons values(1,'Rubin',28);
+insert into persons values(null,'Rahul',27);
+
+--- foreign key
+create table customer(
+	cust_id int,
+    name varchar(50),
+    age int,
+    constraint pk primary key(cust_id)
+);
+
+create table orders(
+	order_id int,
+    order_num int,
+    customer_id int,
+    constraint pk primary key(order_id),
+    constraint fk foreign key(customer_id) references customer(cust_id)
+);
+
+insert into customer values(1,'Rubin',29);
+insert into customer values(2,'Rahul',30);
+
+select * from customer;
+
+insert into orders values(1001,20, 1);
+insert into orders values(1002,30, 2);
+insert into orders values(1004,35, 5);
+select * from orders;
+
+select * from persons;
+
+--- difference between drop and truncate
+truncate table persons;
+drop table persons;
+
+create table if not exists employee(
+	id int,
+    name varchar(50),
+    age int,
+    hiring_date date,
+    salary int,
+    city varchar(50)
+);
+select * from employee;
+
+--- count
+select count(*) from employee;
+select count(1000) from employee;
+
+--- alias
+select count(*) as total_row_count from employee;
+
+--- selected columns
+select name, salary from employee;
+
+select name as employee_name,
+		salary as employee_salary
+from employee;
+
+--- distinct
+select count(distinct(hiring_date)) as count_of_hiring_date from employee;
+
+select	id,
+		name,
+        salary as old_salary,
+        salary*1.2 as new_salary
+from employee;
+        
+select * from employee;
+
+--- update
+update employee set age=20 where name='B';
+
+update employee set age=25, salary = 1.2 * salary;
+
+--- selective update
+select * from employee where hiring_date='2018-06-18';
+
+update employee set salary=80000 where hiring_date='2018-06-18';
+
+--- selective delete
+delete from employee where hiring_date='2022-02-23';
+
+--- auto increment
+create table auto_inc_emp(
+	id int auto_increment,
+    name varchar(50),
+    primary key(id)
+);
+
+insert into auto_inc_emp(name) values('Rubin');
+insert into auto_inc_emp(name) values('Rahul');
+
+select * from auto_inc_emp;
+
+--- limit
+select * from employee limit 3;
+
+--- order by
+select * from employee order by salary desc;
+
+
+--- multi level ordering
+insert into employee values(6,'F',28,'2020-02-20',97000,'Shajapur');
+select * from employee order by salary desc, name asc;
+
+
+--- max salary
+select * from employee order by salary desc limit 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
